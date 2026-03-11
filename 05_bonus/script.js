9df3083049c7c89346519c1a906d68d3aa557581
Campocruz <cosmacampo93@yahoo.it>
2026-03-11 19:42:11 +0100

    done

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,13 +6,25 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-
+const d = new Date();
 
 // Dichiara la funzione qui.
-
+const userWelcome = (inputName) => {
+  if (inputName === null) { return }
+  const hour = d.getHours();
+  let internalStr;
+  if (hour > 17) {
+    internalStr = `Buonasera ${inputName} sono le ore ${hour}`;
+  } else if (hour < 13) {
+    internalStr = `Buongiorno ${inputName} sono le ore ${hour}`;
+  } else {
+    internalStr = `Buon Pomeriggio ${inputName} sono le ore ${hour}`;
+  }
+  return internalStr
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const nameWelcome = userWelcome(name);
+console.log(nameWelcome);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
