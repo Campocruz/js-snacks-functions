b5ac2da641f2cdd566be56174f87d05df46ad0e2
Campocruz <cosmacampo93@yahoo.it>
2026-03-11 19:28:51 +0100

    done

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+const countVowels = (inputStr) => {
+  let count = 0;
+  if (inputStr === null) { return }
+  for (let i = 0; i < inputStr.length; i++) {
+    const char = inputStr.charAt(i);
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+      count++
+    }
+  }
+  return count
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const countedVowels = countVowels(word);
+console.log(countedVowels);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
