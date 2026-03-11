c7119c5dad927cfb735c1851463d894984277646
Campocruz <cosmacampo93@yahoo.it>
2026-03-11 19:10:39 +0100

    done

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,20 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+const shortedName = (inputArr) => {
+  console.log('in the function');
+  const outputArr = [];
+  if(inputArr === null) {return}
+  console.log('no return');
+  for(let i = 0; i < inputArr.length; i++){
+    outputArr.push(inputArr[i].charAt(0));
+  }
+  return outputArr
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const shortName = shortedName(names);
+console.log(shortName);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
