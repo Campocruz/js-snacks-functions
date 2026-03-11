13dc18c56d84ced2b7bf96040de4838a5ed1909c
Campocruz <cosmacampo93@yahoo.it>
2026-03-11 19:19:13 +0100

    done

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const checkChar = (inputArr, inputChar) => {
+  const outputArr = [];
+  if (inputArr === null) { return }
+  for (let i = 0; i < inputArr.length; i++) {
+    if (inputArr[i].charAt(0) === inputChar) {
+      outputArr.push(inputArr[i]);
+    }
+  }
+  return outputArr
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const checkedChar = checkChar(names, 'A');
+console.log(checkedChar);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
